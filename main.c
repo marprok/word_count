36ab2559c7f82d78b964105b235e6163815fee4a
marprok <mariosprokopakis@gmail.com>
2021-01-09 19:43:19 +0200

    comments

--- main.c
+++ main.c
@@ -14,8 +14,10 @@
 #include <pthread.h>
 
 #define NUM_OF_THREADS 4
+#define OUT_FILE_NAME "out.txt"
 static int out_file;
 
+/* The signal handler used to ignore SIGINT and SIGTERM */
 void signal_handler(int signum)
 {
     if (signum == SIGTERM)
@@ -28,6 +30,7 @@ void signal_handler(int signum)
     signal(signum, signal_handler);
 }
 
+/* Returns 1 if the path is a valid directory, 0 otherwise */
 int validate_dir(const char* path)
 {
     assert(path);
@@ -42,6 +45,11 @@ int validate_dir(const char* path)
     return S_ISDIR(st.st_mode);
 }
 
+/*
+ * Reads all the chars in a file and returns 1 if
+ * all of them where readable and 0 otherwise.
+ * It checks if a file is a text file.
+ */
 int validate_file(uint8_t* buf, size_t size)
 {
     assert(buf && size > 0);
@@ -49,39 +57,33 @@ int validate_file(uint8_t* buf, size_t size)
     size_t i = 0;
     for (; i < size; ++i)
     {
+        /*
+         * If the char is nit a readable ascii and it is not whitespace then
+         * it is a good indication that the file is not a text file.
+         */
         if ((buf[i] < 0x20 || buf[i] > 0x7F) && !isspace(buf[i]))
-        {
             return 0;
-        }
     }
 
     return 1;
 }
 
-size_t count_words(uint8_t* buf, size_t size)
-{
-    assert(buf && size > 0);
-
-    size_t i = 0, count = 0;
-    while (i < size)
-    {
-        while (i < size && isspace(buf[i])) i++;
-
-        if (i < size) count++;
-        while (i < size && !isspace(buf[i])) i++;
-    }
-
-    return count;
-}
-
+/*
+ * This struct represents the arguments that
+ * each thread needs to complete it's job.
+ */
 typedef struct
 {
-    size_t from;
-    size_t count;
-    uint8_t *data;
-    size_t result;
+    size_t from; /* Offsset of the first char to process */
+    size_t count; /* How many chars to process */
+    uint8_t *data; /* The actual char buffer */
+    size_t result; /* the number of words found */
 } work_t;
 
+/*
+ * This is the function that each thread runs.
+ * It counts the number of words in a section of text.
+ */
 void* thread_work(void* args)
 {
     work_t *work = (work_t*)args;
@@ -98,6 +100,11 @@ void* thread_work(void* args)
     return NULL;
 }
 
+/*
+ * This function splits the file into sections
+ * and starts NUM_OF_THREADS threads to count the
+ * number of words in each section.
+ */
 void process_file(const char* file)
 {
     int fd = open(file, O_RDONLY);
@@ -107,6 +114,7 @@ void process_file(const char* file)
         return;
     }
 
+    /* We use stat to get the size of the file in bytes */
     struct stat st;
     if (stat(file, &st) < 0 )
     {
@@ -115,17 +123,29 @@ void process_file(const char* file)
         return;
     }
 
+    /* The bytes of the file */
     uint8_t *data = malloc(sizeof(uint8_t)*st.st_size);
+    /* The line to be writen in the output file */
     char line[256 + 1] = {0};
+    /*
+     * If the read went ok and the file is not a binary file
+     * then we start the processing
+     */
     if (read(fd, data, sizeof(uint8_t)*st.st_size) &&
         validate_file(data, st.st_size))
     {
+        /* The worker threads */
         pthread_t workers[NUM_OF_THREADS];
+        /* The args for each thread */
         work_t work[NUM_OF_THREADS];
+        /* The byte offset inside the file */
         size_t from = 0;
         size_t i = 0;
-        size_t tb = 0;
+        /* Make a guess about the initial load of each thread */
         size_t bytes_to_parse = st.st_size/NUM_OF_THREADS;
+        /* The extra chars to process in case a section ended
+         * in a non whitespace char. We have to do This
+         * in order to avoid any errors in the word counting */
         size_t padding = 0;
         for (; i < NUM_OF_THREADS; ++i)
         {
@@ -141,19 +161,20 @@ void process_file(const char* file)
                     padding++;
                 }
             }
+            /* Initialize the work for each thread */
             work[i].from = from;
             work[i].count =  count;
             work[i].data = data;
             work[i].result =  0;
+            /* Start the thread */
             if (pthread_create(workers+i, NULL, thread_work, work+i) != 0)
             {
                 printf("Error creating the thread!\n");
             }
             from += count;
-            tb += count;
         }
-        printf("size %zu\n", tb);
 
+        /* Wait for each thread to finish its work */
         size_t total_words = 0;
         for (i = 0; i < NUM_OF_THREADS; ++i)
         {
@@ -169,10 +190,15 @@ void process_file(const char* file)
         write(out_file, line, strlen(line));
     }
 
+    /* Clean up */
     free(data);
     close(fd);
 }
 
+/*
+ * This function returns the number if non-directory entries in the
+ * given directory.
+ */
 size_t count_files(const char *path)
 {
     assert(path);
@@ -194,14 +220,19 @@ size_t count_files(const char *path)
 
 int main(int argc, char** argv)
 {
-    if (argc != 2 || !validate_dir(argv[1]))
+    const char* target = ".";
+    if (argc > 1)
+        target = argv[1];
+
+    /* Chech that the given name is a valid directory */
+    if (!validate_dir(target))
     {
         printf("Invalid args!\n");
         exit(1);
     }
 
     /* Create the output file with rw-rw-r-- permissions in octal */
-    out_file = open("out.txt", O_CREAT | O_WRONLY | O_APPEND, 0664);
+    out_file = open(OUT_FILE_NAME, O_CREAT | O_WRONLY | O_APPEND, 0664);
     if (out_file < 0 )
     {
         printf("Failed to create the output file!\n");
@@ -210,8 +241,10 @@ int main(int argc, char** argv)
 
     pid_t parent_pid = getpid();
     size_t file_count = count_files(argv[1]);
+    /* The pids of the working processes */
     pid_t *processes = malloc(sizeof(pid_t)*file_count);
 
+    /* Iterate over the directory and start the forking */
     DIR *dir_handle = opendir(argv[1]);
     struct dirent *dr = NULL;
     char rel_path[1024] = {0};
@@ -223,23 +256,28 @@ int main(int argc, char** argv)
             processes[i] = fork();
             if (!processes[i])
             {
+                /* If it is the child process */
                 sprintf(rel_path, "%s/%s", argv[1], dr->d_name);
                 break; /* Stop forking and start working */
             }
             i++;
         }
     }
+
+    /* We do not need the directory open. */
     closedir(dir_handle);
     if (parent_pid != getpid())
     {
+        /* Deallocate any uneeded resources */
         free(processes);
-
+        /* Start the processing of the file */
         process_file(rel_path);
     }else
     {
+        /* Override the signal handlers */
         signal(SIGINT,signal_handler);
         signal(SIGTERM,signal_handler);
-
+        /* Wait for all the children to complete their job */
         i = 0;
         int status;
         for (; i < file_count; ++i)
